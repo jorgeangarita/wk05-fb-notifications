6772e4d951daeeb45199c7ab76883fccd9b3f439
Jorge <jorgea@coldarmy-lm.corp.yahoo.com>
2014-04-07 11:50:42 -0700

    Initial Commit

---
+++ wk05 fb notificationsTests/wk05_fb_notificationsTests.m
@@ -0,0 +1,34 @@
+//
+//  wk05_fb_notificationsTests.m
+//  wk05 fb notificationsTests
+//
+//  Created by Jorge Angarita on 4/7/14.
+//  Copyright (c) 2014 jorge. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface wk05_fb_notificationsTests : XCTestCase
+
+@end
+
+@implementation wk05_fb_notificationsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
